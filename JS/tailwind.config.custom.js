f22e8467db14d6a35147436c6469ed04362c34c1
Services by EM <140435431+ServicesEM@users.noreply.github.com>
2025-06-12 11:42:41 -0500

    Create tailwind.config.custom.js

---
+++ JS/tailwind.config.custom.js
@@ -0,0 +1,40 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ['Lato', 'sans-serif'],
+        secondary: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        mc: {
+          primary: "#2bb6c1",
+          secondary: "#dc5e97",
+          "accent-1": "#ffd60a",
+          "accent-2": "#6464ff",
+          text: {
+            base: "#212121",
+            soft: "#595959",
+          },
+          bg: {
+            main: "#ffffff",
+            soft: "#f5f5f5",
+          },
+          border: "#e6e6e6",
+        },
+      },
+      borderRadius: {
+        sm: "4px",
+        md: "8px",
+        lg: "16px",
+      },
+      boxShadow: {
+        mc: "0 2px 6px rgba(0, 0, 0, 0.08)",
+      },
+      screens: {
+        "3xl": "1800px",
+        "4xl": "2400px",
+        "5xl": "3000px",
+      },
+    },
+  },
+};
